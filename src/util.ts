abb6de408e2f4f9c0396bb6389a4cbdca39075cf
Matthijs Laan <matthijsln@gmail.com>
2020-09-10 19:12:06 +0200

    fix off by one error

--- src/util.ts
+++ src/util.ts
@@ -39,7 +39,7 @@ function repeat(s: string, count: number): string {
     if (count === 0) {
         return '';
     }
-    for(let i = 0; i < count; i++) {
+    for(let i = 0; i < count-1; i++) {
         str += s;
     }
     return str;
